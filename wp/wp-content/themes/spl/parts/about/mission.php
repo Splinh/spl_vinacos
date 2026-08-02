a8ac30a6e4d15a892ee646ad651c0952c3f2b5bd
Splinh <splworks.info@gmail.com>
2026-08-02 14:32:05 +0700

    fix: replace broken unila image URL in Vision & Mission section with local high-res Vinacos graphic asset

--- wp/wp-content/themes/spl/parts/about/mission.php
+++ wp/wp-content/themes/spl/parts/about/mission.php
@@ -7,7 +7,8 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$is_en = function_exists( 'pll_current_language' ) && 'en' === pll_current_language();
+$is_en   = function_exists( 'pll_current_language' ) && 'en' === pll_current_language();
+$img_url = get_template_directory_uri() . '/static/img/tam-nhin-su-menh-vinacos.png';
 ?>
 
 <section class="about-3-section section-large">
@@ -16,7 +17,8 @@ $is_en = function_exists( 'pll_current_language' ) && 'en' === pll_current_langu
 			<?= $is_en ? 'Vision & Mission' : 'Tầm nhìn & Sứ mệnh' ?>
 		</h2>
 		<div class="image img-cover mt-10" data-aos="fade-up" data-aos-duration="700" data-aos-delay="800">
-			<img class="lozad" src="https://unila.com.vn/wp-content/uploads/2026/04/TAM-NHIN-SU-MENH.jpg" data-src="https://unila.com.vn/wp-content/uploads/2026/04/TAM-NHIN-SU-MENH.jpg" loading="lazy" alt="VINACOS Vision & Mission">
+			<img class="lozad" src="<?= esc_url( $img_url ) ?>" data-src="<?= esc_url( $img_url ) ?>" loading="lazy" alt="VINACOS Vision & Mission">
 		</div>
 	</div>
 </section>
+
